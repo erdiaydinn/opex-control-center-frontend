6cdfb75198630ac97fbac5c06e49ef3462ff64a8
erdiaydinn <160104243+erdiaydinn@users.noreply.github.com>
2026-08-14 19:34:44 +0300

    feat(platform): wire global accessibility preferences into app shell

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,21 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
+import AccessibilityControl from "./platform/preferences/AccessibilityControl.jsx";
+import { PlatformPreferencesProvider } from "./platform/preferences/PlatformPreferencesContext.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <PlatformPreferencesProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <AccessibilityControl />
+          <div id="eay-main-content" tabIndex="-1">
+            <App />
+          </div>
+        </AuthProvider>
+      </BrowserRouter>
+    </PlatformPreferencesProvider>
   </React.StrictMode>
 );

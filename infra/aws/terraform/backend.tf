8082bd65438a910153c06b29731bd2671c4a5d63
erdiaydinn <160104243+erdiaydinn@users.noreply.github.com>
2026-08-22 02:23:40 +0300

    infra: bind production Terraform to locked S3 state

--- infra/aws/terraform/backend.tf
+++ infra/aws/terraform/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "eay-tfstate-600219017658-eu-central-1"
+    key          = "eay/production/platform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
